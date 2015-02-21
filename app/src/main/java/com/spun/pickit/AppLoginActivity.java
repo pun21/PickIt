ff4ad3ba852500e451ad6b3961e664b5d2a924e0
pun21 <pun.szelok@ufl.edu>
2015-02-21 16:53:59 -0500

    added facebook login and guest login buttons

--- app/src/main/java/com/spun/pickit/AppLoginActivity.java
+++ app/src/main/java/com/spun/pickit/AppLoginActivity.java
@@ -1,18 +1,38 @@
 package com.spun.pickit;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.facebook.AppEventsLogger;
+import com.facebook.Session;
 
-public class AppLoginActivity extends ActionBarActivity {
+
+public class AppLoginActivity extends FragmentActivity {
+
+    private MainFragment mainFragment;
+    private static final String TAG = "MainFragment";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if (savedInstanceState == null) {
+            // Add the fragment on initial activity setup
+            mainFragment = new MainFragment();
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, mainFragment)
+                    .commit();
+        } else {
+            // Or set the fragment from restored state info
+            mainFragment = (MainFragment) getSupportFragmentManager()
+                    .findFragmentById(android.R.id.content);
+        }
         setContentView(R.layout.activity_app_login);
     }
 
@@ -39,7 +59,27 @@ public class AppLoginActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //Fb's Insights Dashboard - Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Fb's Insights Dashboard - Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
     public void onClickCreateAccount(View v) {
+        Intent intent = new Intent(this, AccountAdminActivity.class);
+        startActivity(intent);
+    }
+
+    public void onClickGuestLogin(View v) {
         Intent intent = new Intent(this, AccountAdminActivity.class);
         startActivity(intent);
     }
