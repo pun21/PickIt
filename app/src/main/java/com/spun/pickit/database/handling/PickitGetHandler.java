9cf3b279d4af4cb38d3eabf45482ca928c2c722f
jackstine <ecstaticjack@gmail.com>
2015-02-21 17:06:14 -0500

    updated the CRUD Modules

---
+++ app/src/main/java/com/spun/pickit/database/handling/PickitGetHandler.java
@@ -0,0 +1,19 @@
+package com.spun.pickit.database.handling;
+
+/**
+ * Created by jacob_000 on 2/21/2015.
+ */
+public class PickitGetHandler extends DatabaseGet{
+    private static final String URL = "SOMETHING";
+    private String userID;
+
+    //this gets the Pickits of the specified userID
+    public PickitGetHandler(String userID){
+        this.userID = userID;
+    }
+
+    public String getExtension(){
+        String extension = "/"+ this.URL + "?UserID="+this.userID;
+        return extension;
+    }
+}
